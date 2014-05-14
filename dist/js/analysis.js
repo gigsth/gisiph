20e22f7e7110eaaa6afc6da22f720b7c99cf5c20
nwatth <nwatth@gmail.com>
2014-05-15 00:38:58 +0700

    chose graph structure

--- dist/js/analysis.js
+++ dist/js/analysis.js
@@ -3,176 +3,182 @@ google.load('visualization', '1', {packages: ["corechart"]});
 var cDiscover, cChronics, cVillage, cColorFromHypertension, cColorFromDiabetes,cOptionVillage;
 
 $(window).on("resize", function(event) {
-    drawCharts();
+	drawCharts();
 });
 
 $(document).ready(function() {
-    google.setOnLoadCallback(drawCharts);
+	google.setOnLoadCallback(drawCharts);
+
 });
 
-$('a.menu').click(function() {
-    setTimeout(function() {
-        drawCharts();
-    },200);
+$('a.menu,  a.sub-menu').click(function() {
+	setTimeout(function() {
+		drawCharts();
+	},200);
 });
 
 function drawCharts() {
-    var tab_id = $('.tab-pane.active').attr('id');console.log(tab_id);
+	var tab_id = $('.tab-pane.view.active').attr('id');
 
-    $(function() {
-        $('.progress-bar').show();
-        if (tab_id === 'tab1') {
-            callJSON('chronics', drawChronics);
-            callJSON('village', drawVillage);
-            callJSON('discover', drawDiscover);
-        } else if (tab_id === 'tab2') {
-            callJSON('nameVillage',getOptionVillage);
-            callJSON('colorFromHypertension', drawColorFromHypertension);
-        }else if (tab_id === 'tab3') {
-            callJSON('nameVillage',getOptionVillage);
-            callJSON('colorFromDiabetes', drawColorFromDiabetes);
-        };
-    }).promise().done(function() {
-        $('.progress-bar').delay(100).fadeOut(400);
-    });
+	$(function() {
+		$('.progress-bar').show();
+		if (tab_id === 'tab1') {
+			var sub_tab_id = $('#tab1 .tab-pane.active').attr('id');
+
+			if (sub_tab_id === 'tab1-1') {
+				callJSON('chronics', drawChronics);
+			} else if (sub_tab_id === 'tab1-2') {
+				callJSON('village', drawVillage);
+			} else if (sub_tab_id === 'tab1-3') {
+				callJSON('discover', drawDiscover);
+			};
+		} else if (tab_id === 'tab2') {
+			callJSON('nameVillage',getOptionVillage);
+			callJSON('colorFromHypertension', drawColorFromHypertension);
+		}else if (tab_id === 'tab3') {
+			callJSON('nameVillage',getOptionVillage);
+			callJSON('colorFromDiabetes', drawColorFromDiabetes);
+		};
+	}).promise().done(function() {
+		$('.progress-bar').delay(100).fadeOut(400);
+	});
 }
 
 function drawChronics(chronics) {
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'โรค');
-    data.addColumn('number', 'จำนวน');
-    data.addRows(getValues(chronics));console.log(getValues(chronics).join(','));
-    cChronics = new google.visualization.PieChart(document.getElementById('chronics'));
-    cChronics.draw(data, {
-            title: 'สัดส่วนของผู้ป่วยโรคเรื้อรัง',
-            width: '100%',
-            height: '100%'
-      });
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'โรค');
+	data.addColumn('number', 'จำนวน');
+	data.addRows(getValues(chronics));console.log(getValues(chronics).join(','));
+	cChronics = new google.visualization.PieChart(document.getElementById('chronics'));
+	cChronics.draw(data, {
+			title: 'สัดส่วนของผู้ป่วยโรคเรื้อรัง',
+			width: '100%',
+			height: '100%'
+	  });
 }
 
 function drawVillage(village) {
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'หมู่บ้าน');
-    data.addColumn('number', 'เบาหวาน');
-    data.addColumn('number', 'ความดันโลหิตสูง');
-    data.addRows(getValues(village));
-    cVillage = new google.visualization.ColumnChart(document.getElementById('village'));
-    cVillage.draw(data, {
-            title: 'สัดส่วนของผู้ป่วยโรคเรื้อรังในแต่ละหมู่บ้าน',
-            width: '100%',
-            height: '100%'
-      });
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'หมู่บ้าน');
+	data.addColumn('number', 'เบาหวาน');
+	data.addColumn('number', 'ความดันโลหิตสูง');
+	data.addRows(getValues(village));
+	cVillage = new google.visualization.ColumnChart(document.getElementById('village'));
+	cVillage.draw(data, {
+			title: 'สัดส่วนของผู้ป่วยโรคเรื้อรังในแต่ละหมู่บ้าน',
+			width: '100%',
+			height: '100%'
+	  });
 }
 
 function drawDiscover(discover) {
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'ปี');
-    data.addColumn('number', 'เบาหวาน');
-    data.addColumn('number', 'ความดันโลหิตสูง');
-    data.addRows(getValues(discover));
-    cDiscover = new google.visualization.LineChart(document.getElementById('discover'));
-    cDiscover.draw(data, {
-            title: 'จำนวนผู้ป่วยโรคเรื้อรังในแต่ละปี',
-            width: '100%',
-            height: '100%'
-      });
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'ปี');
+	data.addColumn('number', 'เบาหวาน');
+	data.addColumn('number', 'ความดันโลหิตสูง');
+	data.addRows(getValues(discover));
+	cDiscover = new google.visualization.LineChart(document.getElementById('discover'));
+	cDiscover.draw(data, {
+			title: 'จำนวนผู้ป่วยโรคเรื้อรังในแต่ละปี',
+			width: '100%',
+			height: '100%'
+	  });
 }
 
 
 function drawColorFromHypertension(colorFromHypertension) {
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'ระดับความรุนแรง');
-    data.addColumn('number', 'จำนวน');
-    data.addColumn({type: 'string', role: 'style'});
-    data.addRows(getValues(colorFromHypertension));
-    var view = new google.visualization.DataView(data);
-    view.setColumns(
-        [
-            0, 
-            1,
-            { 
-                calc: "stringify",
-                sourceColumn: 1,
-                type: "string",
-                role: "annotation" 
-            },
-            2
-        ]
-    );
-    cColorFromHypertension = new google.visualization.ColumnChart(document.getElementById('colorFromHypertension'));
-    cColorFromHypertension.draw(view, {
-            title: 'จำนวนของผู้ป่วยโรคความดันโลหิตสูงจำแนกตามระดับความรุนแรง',
-            width: '100%',
-            height: '100%',
-            bar: {groupWidth: "70%"},
-            legend: { position: "none" }
-      });
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'ระดับความรุนแรง');
+	data.addColumn('number', 'จำนวน');
+	data.addColumn({type: 'string', role: 'style'});
+	data.addRows(getValues(colorFromHypertension));
+	var view = new google.visualization.DataView(data);
+	view.setColumns(
+		[
+			0, 
+			1,
+			{ 
+				calc: "stringify",
+				sourceColumn: 1,
+				type: "string",
+				role: "annotation" 
+			},
+			2
+		]
+	);
+	cColorFromHypertension = new google.visualization.ColumnChart(document.getElementById('colorFromHypertension'));
+	cColorFromHypertension.draw(view, {
+			title: 'จำนวนของผู้ป่วยโรคความดันโลหิตสูงจำแนกตามระดับความรุนแรง',
+			width: '100%',
+			height: '100%',
+			bar: {groupWidth: "70%"},
+			legend: { position: "none" }
+	  });
 
 }
 
 
 function drawColorFromDiabetes(colorFromDiabetes) {console.log(colorFromDiabetes);
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'ระดับความรุนแรง');
-    data.addColumn('number', 'จำนวน');
-    data.addColumn({type: 'string', role: 'style'});
-    data.addRows(getValues(colorFromDiabetes));
-    var view = new google.visualization.DataView(data);
-    view.setColumns(
-        [
-            0, 
-            1,
-            { 
-                calc: "stringify",
-                sourceColumn: 1,
-                type: "string",
-                role: "annotation" 
-            },
-            2
-        ]
-    );
-    cColorFromDiabetes = new google.visualization.ColumnChart(document.getElementById('colorFromDiabetes'));
-    cColorFromDiabetes.draw(view, {
-            title: 'จำนวนของผู้ป่วยโรคเบาหวานจำแนกตามระดับความรุนแรง',
-            width: '100%',
-            height: '100%',
-            bar: {groupWidth: "70%"},
-            legend: { position: "none" }
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'ระดับความรุนแรง');
+	data.addColumn('number', 'จำนวน');
+	data.addColumn({type: 'string', role: 'style'});
+	data.addRows(getValues(colorFromDiabetes));
+	var view = new google.visualization.DataView(data);
+	view.setColumns(
+		[
+			0, 
+			1,
+			{ 
+				calc: "stringify",
+				sourceColumn: 1,
+				type: "string",
+				role: "annotation" 
+			},
+			2
+		]
+	);
+	cColorFromDiabetes = new google.visualization.ColumnChart(document.getElementById('colorFromDiabetes'));
+	cColorFromDiabetes.draw(view, {
+			title: 'จำนวนของผู้ป่วยโรคเบาหวานจำแนกตามระดับความรุนแรง',
+			width: '100%',
+			height: '100%',
+			bar: {groupWidth: "70%"},
+			legend: { position: "none" }
 
-      });
+	  });
 
 }
 
 
 function callJSON(req, callback) {
-    $.ajax({
-        type: 'POST',
-        url: './dist/php/analysis.php',
-        data: {
-            request: req
-        },
-        contentType: "application/x-www-form-urlencoded;charset=utf-8",
-        success: function(data) {
-            if (data.response == 'success') {
-                callback(data.values);
-            }
-        }
-    });
+	$.ajax({
+		type: 'POST',
+		url: './dist/php/analysis.php',
+		data: {
+			request: req
+		},
+		contentType: "application/x-www-form-urlencoded;charset=utf-8",
+		success: function(data) {
+			if (data.response == 'success') {
+				callback(data.values);
+			}
+		}
+	});
 }
 
 function getValues(v) {
-    var rows = [];
-    $.each(v, function(i, v) {
-        var row = [];
-        $.each(v, function(i, v) {
-            row.push(v);
-        });
-        rows.push(row);
-    });
-    return rows;
+	var rows = [];
+	$.each(v, function(i, v) {
+		var row = [];
+		$.each(v, function(i, v) {
+			row.push(v);
+		});
+		rows.push(row);
+	});
+	return rows;
 }
 
-function getOptionVillage(nameVillage)
-{
-    for(var i=0;i<nameVillage.)
+function getOptionVillage(nameVillage){
+	//for(var i=0;i<nameVillage.);
 }
